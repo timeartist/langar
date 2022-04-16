7cf5199710304e1578b451f460d5c86f273aac70
Adi Wabisabi <adi.wabisabi@redis.com>
2022-04-16 11:08:42 -0600

    finish up #1 beginnings of #4

--- langar/app.py
+++ langar/app.py
@@ -1,12 +1,27 @@
-from json import dumps
-
 from flask import Flask, render_template, request
 
-from langar.models import Client
+from langar.models import Client, CheckIn
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/check-in')
+def check_in_get():
+    query = request.args.get('query')
+    id = request.args.get('id')
+
+    if id is not None:
+        results = Client.find(f'@id:{id}')
+        client = results[0]
+        CheckIn(**client)
+    elif query is not None:
+        results = Client.find(query)
+    else:
+        results = None
+    
+    print(results)
+    return render_template('check_in.html', results=results, query=query)
+    
 @app.route('/register')
 def register_get():
     return render_template('register.html')
@@ -19,4 +34,5 @@ def register_post():
     return render_template('register.html', success=True)
 
 def run():
+    Client.batch_from_json()
     app.run(debug=True)
--- langar/models.py
+++ langar/models.py
@@ -1,9 +1,49 @@
+from uuid import uuid4
 from json import loads, dumps
+from csv import DictReader, DictWriter
+from datetime import datetime
+from glob import glob
 
+
+from redis import StrictRedis
+from redis.exceptions import ResponseError
+from redis.commands.json.path import Path
+from redis.commands.search.field import TextField
+from redis.commands.search.query import Query
+from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+
+R = StrictRedis()
+
+_checkin_key = lambda id: f'checkin:{id}'
+_checkin_file_headers = ['id', 'zip_code', 'dob', 'adults', 'minors', 'seniors']
+_client_key = lambda id: f'client:{id}'
+_client_file_headers =  keys = ['id', 'first_name', 'last_name', 'dob', 'zip_code', 'phone_number', 'email_address', 'homelessness', 'adults', 'minors', 'seniors']
+
+def _client_index():
+    idx = R.ft(_client_key('idx'))
+    try:
+        idx.info()
+        return idx
+    except ResponseError:
+        pass
+
+    idx.create_index((
+        TextField('$.id', as_name='id'),
+        TextField('$.first_name', as_name='first'),
+        TextField('$.last_name', as_name='last')
+    ), definition=IndexDefinition(prefix=[_client_key('')], index_type=IndexType.JSON))
+
+    return idx
+
+def _read_clients_csv():
+    with open('data/clients.csv', 'r') as f:
+        rows = DictReader(f)
+        return [row for row in rows]
 
 class Client:
     def __init__(self, first_name, last_name, dob, zip_code, adults, minors, seniors,
                  phone_number=None, email_address=None, homelessness='false') -> None:
+        self.id = str(uuid4()).replace('-', '')
         self.first_name = first_name
         self.last_name = last_name
         self.dob = dob
@@ -14,13 +54,74 @@ class Client:
         self.adults = int(adults)
         self.minors = int(minors)
         self.seniors = int(seniors)
+        
+
+    @staticmethod
+    def batch_from_json():
+        _client_index()
+
+        clients = _read_clients_csv()
+
+        with R.pipeline(transaction=False) as pipe:
+            for client in clients:
+                client['id'] = client['id'].replace('-', '')
+                pipe.json().set(_client_key(client['id']), Path.root_path(), client)
+            
+            pipe.execute()
+
+    @staticmethod
+    def find(query):
+        idx = _client_index()
+        query = Query(query)
+        return _deserialize_results(idx.search(query))
+
 
     def save(self):
-        with open('data/clients.json', 'r') as f:
-            clients = loads(f.read())
-        
-        print(clients)
-        clients.append(self.__dict__)
+        clients = _read_clients_csv()
 
-        with open('data/clients.json', 'w') as f:
-            f.write(dumps(clients))
+        clients.append(self.__dict__)
+        R.json().set(_client_key(self.id), Path.root_path(), self.__dict__)
+
+        with open('data/clients.csv', 'w') as f:
+            dw = DictWriter(f, _client_file_headers)
+            dw.writeheader()
+            dw.writerows(clients)
+
+class CheckIn:
+    def __init__(self, id, zip_code, dob, adults, minors, seniors, **kwargs) -> None:
+        self.date = datetime.today().isoformat().split('T')[0]
+        self.file = f'data/distributions/distribution-{self.date}.csv'
+        self.id = id
+        self.zip_code = zip_code
+        self.dob = dob
+        self.adults = adults
+        self.minors = minors
+        self.seniors = seniors
+
+        R.json().set(_checkin_key(self.id), Path.root_path(), self.__dict__)
+
+        if not glob(self.file):
+            open(self.file, 'w+')
+            checkins = []
+        else:
+            with open(self.file, 'r') as f:
+                rows = DictReader(f)
+                checkins = [row for row in rows]
+
+        data = self.__dict__.copy()
+        data.pop('date')
+        data.pop('file')
+        checkins.append(data)
+
+        with open(self.file, 'w') as f:
+            dw = DictWriter(f, _checkin_file_headers)
+            dw.writeheader()
+            dw.writerows(checkins)
+
+
+
+
+
+def _deserialize_results(results) -> list:
+    '''turn a list of json at results.docs into a list of dicts'''
+    return [loads(result.json) for result in results.docs]
